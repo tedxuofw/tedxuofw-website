ad2db60458953e52347884cf0c9e5b927866751f
Luke <lukechang.jc@gmail.com>
2015-04-16 22:46:51 -0700

    mockups for display grid, input page

--- app/_assets/javascript/main.js
+++ app/_assets/javascript/main.js
@@ -11,4 +11,3 @@ $(document).ready(function(){
 		//POST this string and date/time combination somewhere
 	}
 });
-
---
+++ tagboard/tagboard.js
@@ -0,0 +1,7 @@
+//div id and corresponding string
+//idea: sorted oldest to newest
+var strings = [{"c1", ""}, {"c2", ""},  {"c3", ""}, {"c4", ""}, {"c5", ""}, {"c6", ""}, {"c7", ""}, {"c8", ""}, {"c9", ""}];
+
+function update(){
+	//every n sec, check for new string, replace oldest with this one
+}
